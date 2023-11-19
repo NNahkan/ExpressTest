1181030aee0827e686b6c60deae735829e569506
NNahkan <hoksuz2424@gmail.com>
2023-11-19 13:20:33 -0500

    Updating prisma

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,30 @@ import "express-async-errors";
 const app = express();
 app.use(express.json());
 // All code should go below this line
+////////////////////////////////////////////////////////////////////////
+app.use(express.json());
+// INDEX ENDPOINT
+app.get('/dogs', async (req, res) => {
+	const dogs = await prisma.dog.findMany()
+	res.send(dogs)
+})
 
+/* 
+app.get("/", (_req, res) => {
+  res.json({ message: "Hello World!" }).status(200); // the 'status' is unnecessary but wanted to show you how to define a status
+});
+ */
+
+
+
+
+////////////////////////////////////////////////////////////////////////
 // all your code should go above this line
 app.use(errorHandleMiddleware);
 
 const port = process.env.NODE_ENV === "test" ? 3001 : 3000;
 app.listen(port, () =>
-  console.log(`
+	console.log(`
 🚀 Server ready at: http://localhost:${port}
 `)
 );
